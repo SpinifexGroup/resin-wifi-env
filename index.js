4c202c23452fe6ffab89aa2711c2d523048c9dc4
Jeremy Bunting <buntingj@gmail.com>
2016-07-27 22:02:24 -0700

    Reworked to use a single ‘wifi’ env var

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ const utils = require('./utils');
 const connman = require('./connman');
 
 var setup = () => {
-    if (process.env.SSID === undefined || process.env.PSK === undefined) {
-        console.warn('resin-wifi-env requires the SSID and PSK environment vars to be set. Exiting...');
+    if(process.env.WIFI === undefined || process.env.WIFI.length < 1){
+        console.warn('resin-wifi-env requires the WIFI env var to be set with "SSID|PSK". Exiting...');
         return process.exit();
     }
+    var creds = process.env.WIFI.split('|');
+    var ssid = creds[0];
+    var psk = creds[1];
     var data = `
 [service_home_ethernet]
 Type = ethernet
@@ -16,28 +19,29 @@ Nameservers = 8.8.8.8,8.8.4.4
 
 [service_home_wifi]
 Type = wifi
-Name = ${process.env.SSID}
-Passphrase = ${process.env.PSK}
+Name = ${ssid}
+Passphrase = ${psk}
 Nameservers = 8.8.8.8,8.8.4.4`;
 
-    console.info(`SSID::'${process.env.SSID}' PSK::'${process.env.PSK}'`);
+    console.info(`SSID::'${ssid}' PSK::'${psk}'`);
     utils.durableWriteFile(config.connmanConfig, data).then(() => {
         console.info(`conf file written::${config.connmanConfig}`);
-    }).delay(1000)
-    .then(()=>{
-        connman.waitForConnection(15000)
     })
-    .then(()=>{
-        console.info('connman connected!')
-    })
-    .then(()=>{
-        console.info(`writing to persistent config::${config.persistentConfig}`);
-        utils.durableWriteFile(config.persistentConfig, data)
-    })
-    .then(()=>{
-        console.info('wifi config complete, exiting...');
-        process.exit();
-    }).catch((e)=>{
+        .delay(1000)
+        .then(()=>{
+            connman.waitForConnection(15000)
+        })
+        .then(()=>{
+            console.info('connman connected!')
+        })
+        .then(()=>{
+            console.info(`writing to persistent config::${config.persistentConfig}`);
+            utils.durableWriteFile(config.persistentConfig, data)
+        })
+        .then(()=>{
+            console.info('wifi config complete, exiting...');
+            process.exit();
+        }).catch((e)=>{
         console.log('error writing file', e);
     })
 };
