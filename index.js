5fcc9b15c30f5e1048d47fa646dee7ad3c79ec05
Jeremy Bunting <buntingj@gmail.com>
2016-07-27 21:54:42 -0700

    dropped the 'SSID' and 'PSK' pair for single 'WIFI' var to prevent potentially loading bad vars

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const Promise = require('bluebird');
 const fs = Promise.promisifyAll(require('fs'));
 const config = require('./config');
 const utils = require('./utils');
+const connman = require('./connman');
 
 var setup = () => {
-    if (process.env.SSID === undefined || process.env.PSK === undefined) {
-        console.warn('resin-wifi-env requires the SSID and PSK environment vars to be set. Exiting...');
+    if(process.env.WIFI === undefined || process.env.WIFI.length < 1){
+        console.warn('resin-wifi-env requires the WIFI env var to be set with "SSID|PSK". Exiting...');
         return process.exit();
     }
+    var creds = process.env.WIFI.split('|');
+    var ssid = creds[0];
+    var psk = creds[1];
     var data = `
 [service_home_ethernet]
 Type = ethernet
@@ -15,17 +19,31 @@ Nameservers = 8.8.8.8,8.8.4.4
 
 [service_home_wifi]
 Type = wifi
-Name = ${process.env.SSID}
-Passphrase = ${process.env.PSK}
+Name = ${ssid}
+Passphrase = ${psk}
 Nameservers = 8.8.8.8,8.8.4.4`;
 
-    console.info(`SSID set to '${process.env.SSID}'`);
-    console.info(`PSK set to '${process.env.PSK}'`);
+    console.info(`SSID set to '${ssid}'`);
+    console.info(`PSK set to '${psk}'`);
+
     utils.durableWriteFile(config.connmanConfig, data).then(() => {
         console.info(`file written to ${config.connmanConfig}...exiting`);
+    })
+    .delay(1000)
+    .then(()=>{
+        connman.waitForConnection(15000)
+    })
+    .then(()=>{
+        console.info('connected!')
+    })
+    .then(()=>{
+        utils.durableWriteFile(config.persistentConfig, data)
+    })
+    .then(()=>{
+        console.info('config complete, exiting...');
         process.exit();
     }).catch((e)=>{
-        console.log('error writing file', e)
+        console.log('error writing file', e);
     })
 };
 
